3d0d352af0b1e90e7389f2d16008147f6f8b095d
Swarup Kharul <swarupkharulsk@gmail.com>
2022-08-30 23:45:59 +0530

    feat: add upi id

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 const name = process.env.NAME
+const upi_id = process.env.UPI_ID
 
 app.get('/', (req, res) => res.send('Success!'))
-
-app.get('/test', (req, res) => res.redirect(`https://${name}.com`))
 
 app.get('/pay/:amount', (req, res) => {
     if (req.method === 'GET') {
         const { amount } = req.params
         console.log(`${name} is paying ${amount}`)
         if (amount)
-            res.redirect(`upi://pay?pa=7828369492@ybl&amp;pn=${name}&amp;cu=INR&amp;am=${amount}`)
+            res.redirect(`upi://pay?pa=${upi_id}&amp;pn=${name}&amp;cu=INR&amp;am=${amount}`)
         else
-            res.redirect(`upi://pay?pa=7828369492@ybl&amp;pn=${name}&amp;cu=INR`)
+            res.redirect(`upi://pay?pa=${upi_id}&amp;pn=${name}&amp;cu=INR`)
     }
 })
 
